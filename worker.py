bf1db8b2e7b033acf02cbeb6cd6182d7d742470a
Sarah Barron <123576508+s-barron@users.noreply.github.com>
2023-12-13 15:59:57 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,18 +1,24 @@
 from flask import Flask
 from flask import request
+from flask import render_template
+from google.cloud import secretmanager
 import requests
 import os
 import json
 app = Flask(__name__)
 
-def get_api_key() -> str:
-    secret = os.environ.get("COMPUTE_API_KEY")
-    if secret:
-        return secret
-    else:
-        #local testing
-        with open('.key') as f:
-            return f.read()
+def access_secret_version(secret_id, version_id="latest"):
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/1042978963438/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(name=name)
+
+    # Return the decoded payload.
+    return response.payload.data.decode('UTF-8')
       
 @app.route("/")
 def hello():
@@ -21,14 +27,14 @@ def hello():
 @app.route("/test")
 def test():
     #return "Test" # testing 
-    return(get_api_key())
+    return(access_secret_version("compute-api-key"))
 
 @app.route("/add",methods=['GET','POST'])
 def add():
   if request.method=='GET':
-    return "Use post to add" # replace with form template
+    return "Use post method"
   else:
-    token=get_api_key()
+    token=access_secret_version("compute-api-key")
     ret = addWorker(token,request.form['num'])
     return ret
 
